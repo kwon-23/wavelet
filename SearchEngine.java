c3126bc09eaae6227e692188f63f695152b2a759
kwon-23 <114560984+kwon-23@users.noreply.github.com>
2022-10-14 16:50:27 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,21 +1,18 @@
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
 
 class Handler implements URLHandler {
-    int num = 0;
+    private List<String> words = new ArrayList<>();
+    private List<String> matchWords = new ArrayList<>();
 
-    public String handleRequest(URI url){
-        ArrayList<String> words = new ArrayList<String>();
-        ArrayList<String> matchWords = new ArrayList<String>();
-        String result = "";
+    public String handleRequest(URI url){  
         if (url.getPath().equals("/")) {
             if (words.size() == 0){
-                return String.format("Enter some words with /add?s=.");
+                return String.format("Enter some words with /add?s=");
             }
             else {
-                // Maybe print out all the words? not quite sure how just yet
-                //for(int i = 0; i < words.size(); i++){}
                 return String.format("Enter something to search with /search?s=.");
             }
         }
@@ -24,7 +21,7 @@ class Handler implements URLHandler {
             if(url.getPath().contains("/add")){
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
-                    words.add((parameters[1]));
+                    words.add(parameters[1]);
                     return String.format("%s", parameters[1]);
                 }
             }
@@ -32,16 +29,20 @@ class Handler implements URLHandler {
                 String[] parameters = url.getQuery().split("=");
                 if(parameters[0].equals("s")) {
                     for(int x = 0; x < words.size(); x++){
-                        if((words.get(x)).contains((String)(parameters[1])) == true){
+                        if((words.get(x)).contains((parameters[1])) == true){
                             matchWords.add(words.get(x));
                         }
                     }
+                    String result = "";
                     for(int y = 0; y < matchWords.size(); y++){
                         result += matchWords.get(y);
                     }
                     matchWords.clear();
                     return String.format("%s", result);
                 }
+            }
+            else if(url.getPath().contains("/size")){
+                return String.format(" %d", words.size());
             }
             return "404 Not Found";
         }
